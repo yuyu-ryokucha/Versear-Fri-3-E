87ec3090064a56d9ef95a4f5290ed5dd59316fd4
YUYU <110108254+yuyu-ryokucha@users.noreply.github.com>
2024-07-31 10:20:38 +0900

    sns

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/info')
+def info():
+    return render_template("info.html")
+
 if __name__== "__main__":
     app.debug = True
     app.run()
